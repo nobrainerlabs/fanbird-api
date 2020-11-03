5e058b51937aa76d23cced9da169ab3c1c4407ad
jae lee <jaequery@gmail.com>
2020-11-02 17:11:15 -0800

    test

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('docs', app, document);
+  SwaggerModule.setup('docs', app, document); //
 
   await app.listen(process.env.VIRTUAL_PORT);
 
